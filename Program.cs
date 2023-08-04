ecb5021f1b72cf6c94a718994ed222698ce9644d
Alex <gta.5.98@mail.ru>
2023-08-04 19:47:44 +0300

    Change in Class Program.cs

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace GitTestProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void Methode()
+        {
+            // Hello git
+        }
+    }
+}
